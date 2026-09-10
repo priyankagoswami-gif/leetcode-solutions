7ea45ab76c550985d616490098ca94988e953606
Priyanka Goswami <priyankagoswami1121@gmail.com>
2026-09-10 11:58:39 +0530

    Time: 5 ms (82.14%), Space: 12.5 MB (21.75%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def romanToInt(self, s):
+        roman = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+
+        ans = 0
+
+        for i in range(len(s)):
+            if i + 1 < len(s) and roman[s[i]] < roman[s[i+1]]:
+                ans -= roman[s[i]]
+            else:
+                ans += roman[s[i]]
+
+        return ans
+obj = Solution ()
+Result = obj.romanToInt("MCMIV")
+print(Result)
